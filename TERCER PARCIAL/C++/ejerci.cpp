2d881497e730fe1dcea361052b6627b62ac8369a
anthony sanchez <anthony.sanchez@pucese.edu.ec>
2020-02-05 17:29:04 -0500

    Examen parcial 3 y otros ejercicios de digitos y palabras

---
+++ TERCER PARCIAL/C++/ejerci.cpp
@@ -0,0 +1,11 @@
+void main()
+{
+    int a,b,c;
+    cout<<"Ingrese A:";
+    cin>>a;
+    cout<<"Ingrese B:";
+    cin>>b;
+    c=a+b;
+    cout<<"La suma es:"<<c;
+    getch();
+}
---
+++ TERCER PARCIAL/cuentapalabrasa/texto.bat
@@ -0,0 +1,5 @@
+El día de ayer, hizo mucho calor. Luis estuvo estudiando para el examen
+Daniel compró una camisa que le quedó muy pequeña.
+La PC de Carla s del año 2008 y está muy lenta.
+Los elementos de una PC son: hardware y software.
+El teclado de la PC está dañado, porque le cayó mucha agua.
